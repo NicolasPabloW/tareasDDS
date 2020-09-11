fc35bc06ef2953346bedcb30f91c32a4392cf119
NicolasPabloW <nicokido2011@gmail.com>
2020-09-11 18:56:29 -0300

    Update Paleta.java

--- src/main/java/domain/Paleta.java
+++ src/main/java/domain/Paleta.java
@@ -1,1 +1,11 @@
-kutj yhtgrfd
+public class Paleta {
+	public String nombre ;
+	public String codigoDeColor;
+	public String codigo ;
+	public String descripcionDeColor ;
+	public Float grosor ;
+	
+	public Jugador jugador;
+	public Constructor constructor ;
+	
+}
