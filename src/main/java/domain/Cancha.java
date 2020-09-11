c494ee03ce9df26c7c675800380ffa87a54d312d
NicolasPabloW <nicokido2011@gmail.com>
2020-09-11 18:58:21 -0300

    Update Cancha.java

--- src/main/java/domain/Cancha.java
+++ src/main/java/domain/Cancha.java
@@ -1,7 +1,7 @@
 public class Cancha {
 	public {Partido} partidos;
 	public String nombre ;
-	public String DescripcionDeColor;
-	public String CodigoDeColor ;
+	public String descripcionDeColor;
+	public String codigoDeColor ;
 	public Bool reservada = false ;
 }
